ef573694baeac89af98e9fbc000c67d7b29cb666
0WN463 <>
2025-03-01 23:42:08 +0800

    Add seed data for standard dishes

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ end
 
 
 
-Section.find_or_create_by!(identifier: "unmodifiable_section") do |section|
+standard_section = Section.find_or_create_by!(identifier: "unmodifiable_section") do |section|
   section.label = "Standard Dishes"
   section.description = "Find all our dishes here!"
   MenuSection.create!(
@@ -33,3 +33,39 @@ Section.find_or_create_by!(identifier: "modifiable_section") do |section|
   section.description = "Build your own dish here!"
 end
 
+
+Item.find_or_create_by!(identifier: "saba") do |item|
+  #item.type = "Product"
+  item.label = "Saba Shioyaki"
+  item.description = "Roasted salted Saba fish"
+  item.price = 10.90
+
+  SectionItem.create!(
+    section: standard_section,
+    item: item
+  )
+end
+
+Item.find_or_create_by!(identifier: "sanma") do |item|
+  #item.type = "Product"
+  item.label = "Sanma Shioyaki"
+  item.description = "Roasted salted Sanma fish"
+  item.price = 12.90
+
+  SectionItem.create!(
+    section: standard_section,
+    item: item
+  )
+end
+
+Item.find_or_create_by!(identifier: "yakisobi") do |item|
+  #item.type = "Product"
+  item.label = "Yaki Soba"
+  item.description = "Stir-fried soba noodles. Commonly seen in festivals"
+  item.price = 8.90
+
+  SectionItem.create!(
+    section: standard_section,
+    item: item
+  )
+end

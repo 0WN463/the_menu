07120a980c4bd511c17210d92f8b7b74414be17b
0WN463 <>
2025-03-01 22:47:14 +0800

    rails g graphql:mutation_create menu

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :menu_create, mutation: Mutations::MenuCreate
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"

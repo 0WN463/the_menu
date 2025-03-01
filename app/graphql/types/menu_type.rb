73b465a7e2ef2342f45f0c739c38b985eef7f1a9
0WN463 <>
2025-03-01 12:47:38 +0800

    Implement graphql to fetch all menu

---
+++ app/graphql/types/menu_type.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Types
+  class MenuType < Types::BaseObject
+    field :id, ID, null: false
+    field :identifier, String, null: false
+    field :label, String, null: false
+    field :state, String, null: false
+    field :start_date, GraphQL::Types::ISO8601Date
+    field :end_date, GraphQL::Types::ISO8601Date
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end

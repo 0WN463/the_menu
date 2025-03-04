3bb0874fff2d72665f9d26855bdf014fe95d441e
0WN463 <>
2025-03-05 00:30:08 +0800

    Add graphQL ability to add section to menu

---
+++ app/graphql/mutations/menu_section_create.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Mutations
+  class MenuSectionCreate < BaseMutation
+    description "Add a section to a menu"
+
+    field :menu_section, Types::MenuSectionType, null: false
+
+    argument :menu_id, ID, required: true
+    argument :section_id, ID, required: true
+
+    def resolve(menu_id:, section_id:)
+      menu = ::Menu.find_by(identifier:menu_id)
+      section= ::Section.find_by(identifier:section_id)
+      menu_section = MenuSection::new(menu:, section:)
+      raise GraphQL::ExecutionError.new "Error adding menu to section", extensions: menu_section.errors.to_hash unless menu_section.save!
+
+      { menu_section: menu_section }
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,8 +2,10 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :add_section_to_menu, mutation: Mutations::MenuSectionCreate
     field :menu_delete, mutation: Mutations::MenuDelete
     field :menu_create, mutation: Mutations::MenuCreate
+
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"

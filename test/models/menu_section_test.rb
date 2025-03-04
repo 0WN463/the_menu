3689b3e8444a38caf3164223e1480e105618e473
0WN463 <>
2025-03-04 23:04:39 +0800

    Add test to ensure menu_section is unique

--- test/models/menu_section_test.rb
+++ test/models/menu_section_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class MenuSectionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "disallow adding existing section into menu" do
+    menu = Menu.find_by(identifier: "first_menu")
+    section = Section.find_by(identifier:" first_section")
+    menu_section = MenuSection.create(menu: menu, section: section)
+
+    assert_not_includes MenuSection.all,menu_section, "should not allow duplicated section to be added"
+  end
 end

a28d0ae9c44624ddbdafb9b2b939578e371e9014
0WN463 <>
2025-03-06 01:56:31 +0800

    Add validation on item type

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,6 @@ class Item < ApplicationRecord
   has_many :item_modifier_groups
   has_many :modifier_groups, through: :item_modifier_groups
 
-  validates :identifier, uniqueness: true
+  validates :identifier, uniqueness: true, presence: true
+  validates :item_type, inclusion: { in: %w(component product) }, presence: true
 end

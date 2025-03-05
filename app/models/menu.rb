70a0534d7f160eac1f5be2c3d59f8f37a1d91525
0WN463 <>
2025-03-06 02:05:52 +0800

    Add various presence validations on fields

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,6 +2,7 @@ class Menu < ApplicationRecord
   has_many :menu_sections
   has_many :sections, through: :menu_sections
 
-  validates :identifier, uniqueness: true
+  validates :identifier, uniqueness: true, presence: true
+  validates :label, presence: true
   validates :state, inclusion: { in: %w(draft published) }, presence: true
 end

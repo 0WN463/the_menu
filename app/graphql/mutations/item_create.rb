031a3bf64516827eff5f8c0f2b1e9a582885f586
0WN463 <>
2025-03-15 11:52:28 +0800

    Add image_url to model

--- app/graphql/mutations/item_create.rb
+++ app/graphql/mutations/item_create.rb
@@ -11,13 +11,14 @@ module Mutations
     argument :item_type, String, required: false
     argument :description, String, required: false
     argument :price, Float, required: false
+    argument :image_url, String, required: false
 
     def resolve(identifier:, label:, item_type: "product", description: "", price: 0)
       if item_type != "product" && item_type != "component" then
         raise GraphQL::ExecutionError.new "invalid item type"
       end
 
-      item = ::Item.new(identifier:, label:, item_type:, description:, price:)
+      item = ::Item.new(identifier:, label:, item_type:, description:, price:, image_url:)
 
       raise GraphQL::ExecutionError.new "Error creating item", extensions: item.errors.to_hash unless item.save
 

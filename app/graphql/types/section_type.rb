1997cf6d261cf0202d8ca809350d5e50147129e2
0WN463 <>
2025-03-17 02:44:19 +0800

    Fix typo which causes items to not be sorted by display_order

--- app/graphql/types/section_type.rb
+++ app/graphql/types/section_type.rb
@@ -9,9 +9,9 @@ module Types
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :items, [ Types::ItemType ], null: false
-  end
 
     def items
       SectionItem.where(section_id: object.id).order("display_order ASC").to_a.map { |s| s.item }
     end
+  end
 end
